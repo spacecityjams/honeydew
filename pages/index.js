77c5000f6e352f8f7e3a4a46407c5e6abadde769
jams <james@spacecityjams.com>
2024-05-14 19:19:20 -0500

    working POC, but a lot of room to refactor

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Index = () => {
 
     const [listItem, setListItem] = useState('');
+    const [toDoList, setToDoList] = useState([]);
 
     const handleChange = (e) => {
         e.preventDefault()
@@ -10,12 +11,45 @@ const Index = () => {
         setListItem(e.target.value)
     }
 
+    const handleAdd = (e) => {
+        e.preventDefault();
+
+        setToDoList([
+            listItem,
+            ...toDoList
+        ])
+
+        setListItem('');
+    }
+
+    const handleCompleteTask = (todo) => {
+        const updateArr = toDoList.filter(toDoItem => toDoList.indexOf(toDoItem) != toDoItem.indexOf(todo))
+
+        setToDoList(updateArr);
+    }
+
     return (
         <div>
             <h3>To-Do List</h3>
             <form>
-                <input type='text' onChange={handleChange}/><button>Add</button>
+                <input type='text' value={listItem} placeholder='what needs doing?' onChange={handleChange}/><button onClick={handleAdd}>Add</button>
             </form>
+            <ul style={
+                {
+                    backgroundColor: 'black',
+                    color: 'green'
+                }
+            }>
+                {
+                    toDoList.length >=1 ? toDoList.map((toDo, idx) => {
+                        return <li key={idx}>{toDo}<button onClick={(e) => {
+                            e.preventDefault();
+                            handleCompleteTask(toDo)
+                        }}>Done</button></li>
+                    })
+                    : "all done!"
+                }
+            </ul>
         </div>
     )
 }
